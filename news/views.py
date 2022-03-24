a8c121ca5770bdf5bc3ce73ef7b02bb9b54b91fc
speae <speae147258@daum.net>
2022-03-25 01:04:52 +0900

    Initial commit

---
+++ news/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Article
+
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year': year, 'article_list': a_list}
+    return render(request, 'year_archive.html', context)
